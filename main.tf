97fc78e3dfc75a41efc4a2f8ad62098cb163ffdf
shudarshon <shudarshon@upskillable.com>
2019-06-27 18:20:30 +0600

    included least tf config from ec2 module

--- main.tf
+++ main.tf
@@ -1,17 +1,14 @@
-provider "aws" {
-  region = "${var.aws_region}"
-}
-
-resource "aws_instance" "DevInstanceAWS" {
-  count = "${var.instance_count}"
-  instance_type = "${var.instance_type}"
-  subnet_id = "${var.subnet_id}"
-  ami = "${var.ami_id}"
-  key_name = "${var.ssh_key_name}"
-  tags {
-    Name = "${var.instance_name}"
-  }
-  security_groups = [
-        "${var.security_group}"
-  ]
+module "ec2" {
+  source                = "modules/ec2"
+  aws_region 		    = "us-east-1"
+  instance_type         = "t2.micro"
+  instance_name         = "dev-instance"
+  ami_id                = "ami-0ad4957e82982483c"
+  subnet_id             = "subnet-5dc34072"
+  security_group        = "sg-0a5b207e6ef0ee2cb"
+  ssh_user_name         = "ec2-user"
+  ssh_key_name          = "upskillable"
+  ssh_key_path          = "/home/chaks/.key/upskillable.pem"
+  instance_count        = 1
+  dev_host_label        = "dev"
 }
